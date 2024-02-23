86bc165c28ef5a528a34bce9576163cca0fc63e8
sar1 <129823338+sar369@users.noreply.github.com>
2024-02-23 12:08:01 -0500

    Add files via upload

---
+++ wifi_crack.py
@@ -0,0 +1,59 @@
+import subprocess
+import os
+
+while True:
+    os.system('clear')
+    print("""
+                                                                                                      
+I8,        8        ,8I 88    ad88 88         ,ad8888ba,                                   88         
+`8b       d8b       d8' ""   d8"   ""        d8"'    `"8b                                  88         
+ "8,     ,8"8,     ,8"       88             d8'                                            88         
+  Y8     8P Y8     8P   88 MM88MMM 88       88            8b,dPPYba, ,adPPYYba,  ,adPPYba, 88   ,d8   
+  `8b   d8' `8b   d8'   88   88    88       88            88P'   "Y8 ""     `Y8 a8"     "" 88 ,a8"    
+   `8a a8'   `8a a8'    88   88    88       Y8,           88         ,adPPPPP88 8b         8888[      
+    `8a8'     `8a8'     88   88    88        Y8a.    .a8P 88         88,    ,88 "8a,   ,aa 88`"Yba,   
+     `8'       `8'      88   88    88         `"Y8888Y"'  88         `"8bbdP"Y8  `"Ybbd8"' 88   `Y8a  
+                                                                                                      
+                                                                                                      
+                                                                                         
+                                                                                          --- by sar1            
+""")
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
+    print(">>Select Option by Entering the following number<<\n")
+    print("1. Wifi Adptor to Montor mode")
+    print("2. Wifi Scan && Attack")
+    print("3. Wifi Deauthentication Attack\n")
+    option = int(input("Choose the option : "))
+    os.system('clear')
+
+    if option == 1:
+        print("Adptor Interfaces: ")
+        print("1.wlan0")
+        print("2.Wlan0mon")
+
+        adptor = int(input("Choose Your Adptor Interface: "))
+        if adptor == 1:
+            result = subprocess.run(["bash","wifi_crack.sh"] ,capture_output=True, text=True)
+            if result.returncode == 0:
+                if result.stdout:
+                    print("Output:")
+                    print(result.stdout)
+                    print(">>> Changed to monitor mode succesfully <<<\n")
+                else:
+                    print("ERROR: Failed to change to monitor mode. Recheck the adptor and try.")
+                adptor_return = int(input("Press 1 to Continue or Ctrl+z to quit: "))
+                if adptor_return == 1:
+                    continue
+                else:
+                    break
+            else:
+                print("Error: Check if your adapter is connected properly and Retry again")
+                print(result.stderr)
+                break
+    elif option == 2:
+      #s.system('clear')
+      subprocess.call(["airodump-ng", "wlan0"])
+      
+    else:
+        print("Invalid option. Please choose a valid option.")
+        break
