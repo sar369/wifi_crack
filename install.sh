c29c80762a5dc483461f47cf8daf4c7ada01e908
sar1 <129823338+sar369@users.noreply.github.com>
2024-02-25 15:11:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo apt update && apt upgrade -y
 sudo apt install python && apt install python3 -y
 sudo apt install python3-pip -y
+sudo pip install termcolor
 sleep 1 
 pip install tqdm
 sleep 3
