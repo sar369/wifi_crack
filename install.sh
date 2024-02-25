734c8bcb2bd7202ea14a702296c34a4ce6dac799
sar1 <129823338+sar369@users.noreply.github.com>
2024-02-25 10:51:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ sudo apt update && apt upgrade -y
 sudo apt install python && apt install python3 -y
 sudo apt install python3-pip -y
 sudo pip install termcolor
+sudo apt-get install gnome-terminal
+which gnome-terminal
+export PATH="/usr/bin:$PATH"
 sleep 1 
 pip install tqdm
 sleep 3
