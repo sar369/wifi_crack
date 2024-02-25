13056ca4c6fed2257752b8c0f660c835cfeb4685
sar1 <129823338+sar369@users.noreply.github.com>
2024-02-25 15:36:05 +0530

    Update wifi_crack.py

--- wifi_crack.py
+++ wifi_crack.py
@@ -28,7 +28,7 @@ I8,        8        ,8I 88    ad88 88         ,ad8888ba,                        
 											  --- by sar1 
 	     
 """,green)
-	print(text)
+    print(text)
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
     text = colored(">>Select Option by Entering the following number<<", "yellow")
     print(text)
